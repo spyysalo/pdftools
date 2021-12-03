8b5c42c5c9021291b554c53066b4f57f976a4af0
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2021-12-03 19:54:00 +0200

    Avoid combinatorial explosion

--- filterpagenumbers.py
+++ filterpagenumbers.py
@@ -19,6 +19,18 @@ logger = logging.getLogger(os.path.basename(__file__))
 def argparser():
     ap = ArgumentParser()
     ap.add_argument(
+        '--min-page-number-ratio',
+        type=float,
+        default=0.1,
+        help='minimum pageno/page ratio'
+    )
+    ap.add_argument(
+        '--quiet',
+        default=False,
+        action='store_true',
+        help='reduced output'
+    )
+    ap.add_argument(
         '--debug',
         default=False,
         action='store_true',
@@ -38,6 +50,11 @@ class PageNumberCandidate:
         self.number = number
         self.shape = shape
         self.line = line
+
+    def group_key(self, roundto=10):
+        """Return a key for grouping candidates into compatible subsets."""
+        lly = round(self.line.bbox.lly/roundto)*roundto
+        return (self.shape, self.line.fonts[0], lly)
 
     def __lt__(self, other):
         # check both page index and the number so that the LIS
@@ -86,6 +103,15 @@ def page_number_candidates(document):
     return candidates
 
 
+def _subsequence_count(seq, candidate_map):
+    if not seq:
+        return 1
+    else:
+        first, rest = seq[0], seq[1:]
+        count, total = len(candidate_map[first]), 0
+        return count * _subsequence_count(rest, candidate_map)
+
+
 def _subsequences(seq, candidate_map):
     # Recursive implementation for candidate_sequences()
     if not seq:
@@ -97,12 +123,17 @@ def _subsequences(seq, candidate_map):
                 yield [c] + s
 
 
-def candidate_sequences(sequence, candidates):
-    # Map (page_index, number) to candidates
+def candidate_sequences(sequence, candidates, docid):
+    # Map (page_index, number) to matching candidate sequences
     candidates_by_index_and_number = defaultdict(list)
     for c in candidates:
         candidates_by_index_and_number[(c.page_index, c.number)].append(c)
-    # TODO this risks a combinatorial explosion
+    count = _subsequence_count(sequence, candidates_by_index_and_number)
+    if count > 1000000:
+        logger.error(f'not generating {count} candidate sequences in {docid}')
+        return []
+    elif count > 1000:
+        logger.warning(f'generating {count} candidate sequences in {docid}')
     return list(_subsequences(sequence, candidates_by_index_and_number))
 
 
@@ -110,12 +141,12 @@ def find_page_number_lines(document, args):
     candidates = page_number_candidates(document)
     if not candidates:
         return []
+    logger.debug(f'found {len(candidates)} candidates in {document.id}')
 
-    # Group candidates by line shape (e.g. "- <NUM> -") and font into
-    # compatibly-formatted sequences
+    # Group candidates into compatibly-formatted sequences
     candidate_groups = defaultdict(list)
     for c in candidates:
-        candidate_groups[(c.shape, c.line.fonts[0])].append(c)
+        candidate_groups[c.group_key()].append(c)
 
     # Find longest incrementing subsequence of numbers and page indices
     # from each group, and pick longest
@@ -124,12 +155,17 @@ def find_page_number_lines(document, args):
         for k, v in candidate_groups.items()
     }
     lis_key, lis = max(group_lis.items(), key=lambda kv: len(kv[1]))
+    logger.debug(f'page number LIS {len(lis)} in {document.id}')
 
     # There may be more than one set of candidates that could produce
     # the LIS. Get the (page_index, number) from the LIS and find all
     # candidate sequences that can produce it
     lis_sequence = [(c.page_index, c.number) for c in lis]
-    sequences = candidate_sequences(lis_sequence, candidate_groups[lis_key])
+    sequences = candidate_sequences(
+        lis_sequence,
+        candidate_groups[lis_key],
+        document.id
+    )
 
     if not sequences:
         logger.error(f'failed to recreate candidate sequence')
@@ -152,6 +188,8 @@ def main(argv):
 
     if args.debug:
         logger.setLevel(logging.DEBUG)
+    elif args.quiet:
+        logger.setLevel(logging.ERROR)
     else:
         logger.setLevel(logging.INFO)
 
@@ -162,8 +200,15 @@ def main(argv):
             logger.error(f'failed to load {fn}: {e}')
             continue
         lines = find_page_number_lines(document, args)
-        logger.debug(f'removing {len(lines)} lines from {fn}')
-        document.remove_lines(lines)
+        ratio = len(lines)/len(document.pages)
+        if ratio < args.min_page_number_ratio:
+            logger.info(f'no page number sequences found in {fn}')
+        else:
+            logger.info(
+                f'removing {len(lines)} page numbers from '
+                f'{len(document.pages)} pages ({ratio:.1%}) in {fn}'
+            )
+            document.remove_lines(lines)
         print(document.to_freki())
 
 
